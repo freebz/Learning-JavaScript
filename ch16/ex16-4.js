42e8dd8ca9f780f1f0020053d778edc268c88eac
FreeBz <freebz@hananet.net>
2017-08-31 19:49:15 +0900

    16장 Math

---
+++ ch16/ex16-4.js
@@ -0,0 +1,6 @@
+const x = 12;
+x.toString();      // "12" (10진수)
+x.toString(10);	   // "12" (10진수)
+x.toString(16);	   // "c" (16진수)
+x.toString(8);	   // "14" (8진수)
+x.toString(2);	   // "1100" (2진수)
