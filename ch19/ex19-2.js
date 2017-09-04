58038aa45ed6abb5bd1a4d62fb2c7174613b0f37
FreeBz <freebz@hananet.net>
2017-09-04 17:15:10 +0900

    19장 제이쿼리

---
+++ ch19/ex19-2.js
@@ -0,0 +1,8 @@
+const $paras = $('p');
+$paras.length;			// 문단의 수
+typeof $paras;			// "object"
+$paras instanceof $;		// true
+$paras instanceof jQuery;	// true
+
+
+const $newPara = $('<p>Newly created paragraph...</p>');
---
+++ ch19/ex19-5.js
@@ -0,0 +1,16 @@
+function refreshServerInfo() {
+    const $serverInfo = $('.serverInfo');
+    $.get('http://localhost:7070').then(
+	// 성공한 경우
+	function(data) {
+	    Object.keys(data).forEach(p => {
+		$(`[data-replace="${p}"]`).text(data[p]);
+	    });
+	},
+	function(jqXHR, textStatus, err) {
+	    console.error(err);
+	    $serverInfo.addClass('error')
+		.html('Error connectiong to server.');
+	}
+    );
+}
